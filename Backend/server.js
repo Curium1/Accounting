ccca802550c837e7140804267d0a37ecfe24dcf0
Curium1 <patrick.bender@benders.se>
2025-05-27 15:48:14 +0200

    test

--- Backend/server.js
+++ Backend/server.js
@@ -33,6 +33,7 @@ const PORT = process.env.PORT || 5000;
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log('Hi')
 });
 
 // Handle unhandled promise rejections
