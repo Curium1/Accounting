015b2a75467ba0822a70e88ca4e576032f082c41
Curium1 <patrick.bender@benders.se>
2025-05-29 20:41:40 +0200

    tailwind swetup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,10 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      // tells Vite that `@/xxx` → `<projectRoot>/src/xxx`
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
